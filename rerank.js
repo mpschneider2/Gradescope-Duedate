b98be734be26c1eeb56081b3cc151b59afb40620
Matthew Schneider <59853199+mpschneider2@users.noreply.github.com>
2025-08-28 16:30:28 -0400

    first version working?

---
+++ rerank.js
@@ -0,0 +1,3 @@
+window.addEventListener('load', function() {
+    document.querySelector("#assignments-student-table > thead > tr > th.table--complexHeader.sorting-right.sorting.sorting_desc").click();
+});
